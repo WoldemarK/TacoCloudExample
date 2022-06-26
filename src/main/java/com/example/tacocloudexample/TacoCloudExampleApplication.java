048c1861b95abcd41503e9be84bf3e1a56b81544
WoldemarK <kovtynov.vladimir@gmail.com>
2022-06-26 23:54:52 +0300

    Initial commit

---
+++ src/main/java/com/example/tacocloudexample/TacoCloudExampleApplication.java
@@ -0,0 +1,16 @@
+package com.example.tacocloudexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class TacoCloudExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudExampleApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/tacocloudexample/TacoCloudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
